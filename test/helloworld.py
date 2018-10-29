baae28a97cde44883676cc6a70f11e18a21d402d
HuangChenning <hrich1203@gmail.com>
2018-10-30 02:13:13 +0800

    1

--- test/helloworld.py
+++ test/helloworld.py
@@ -1,8 +1,24 @@
+/*
+ * @Author: Huangcn
+ * @Date: 2018-10-30 02:11:56
+ * @LastEditors: Huangcn
+ * @LastEditTime: 2018-10-30 02:12:19
+ * @Description: file content
+ */
+
 # -* - coding: UTF-8 -* -
 # 如果要输出中文，需要指定上面这段内容
 # 如下注释用于指定解释器
 # ! /usr/bin/python
 
+
+/**
+ * @name: *
+ * @test: test font *
+ * @msg: *
+ * @param {type} 
+ * @return: 
+ */
 print("hello,world!")
 print("你好")
 print("hello," + "world" + "!")
---
+++ test/logtest.py
@@ -0,0 +1,20 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2018-10-30 01:49:00
+ * @LastEditTime: 2018-10-30 01:53:10
+ * @LastEditors: your name
+ */
+
+
+
+import logging
+
+logging.debug("This is a debug log.")
+logging.info("This is a info log.")
+logging.warning("This is a warning log.")
+logging.error("This is a error log.")
+logging.critical("This is a critical log.")
+
+
+
